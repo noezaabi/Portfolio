1d132d91c3b632f238e7f97da09f5e7591f30bbf
Noé Zaabi <noe@Noes-MacBook-Pro.local>
2023-08-29 09:50:46 -0700

    Added Clickable projects + master studies

--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,8 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import kanflowImg from "@/public/kanflow.png";
 import doctorImg from "@/public/doctor.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
@@ -44,7 +42,7 @@ export const experiencesData = [
     description:
       "I studied physics in one of the best STEM school in Europe. Thanks to it's polytechnic nature, I also had multiple classes in pure math, computer science and applied sciences.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019 - 2021",
+    date: "2020 - 2023",
   },
   {
     title: "Project Manager @ Junior Entreprise EPFL",
@@ -62,6 +60,14 @@ export const experiencesData = [
     icon: React.createElement(CgWorkAlt),
     date: "2022 - 2023",
   },
+  {
+    title: "Master in Quantum Science and Engineering @ EPFL",
+    location: "Lausanne, Switzerland",
+    description:
+      "I am specializing my master on the study of quantum sciences with a particular focus on quantum computing. This master will allow me to continue to develop my physics knowledge while learning more on theoretical computer science.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2023 - 2025",
+  },
 ] as const;
 
 export const projectsData = [
@@ -71,6 +77,7 @@ export const projectsData = [
       "Full stack web app built on the TRPC stack. It's a kanban board with features like drag and drop, real time updates and authentication.",
     tags: ["Next.js", "Typescript", "TRPC", "Prisma", "Tailwind", "Supabase"],
     imageUrl: kanflowImg,
+    url: "https://github.com/noezaabi/KanFlow",
   },
   {
     title: "Doctor Training App",
@@ -78,27 +85,6 @@ export const projectsData = [
       "One of my flutter project where I had to develop an app for a company that gave courses to doctor in the Middle East.",
     tags: ["Flutter", "Dart", "Library", "Quizz", "Streaming"],
     imageUrl: doctorImg,
+    url: "",
   },
-] as const;
-
-export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "Next.js",
-  "Node.js",
-  "Git",
-  "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
-  "PostgreSQL",
-  "Python",
-  "Django",
-  "Framer Motion",
 ] as const;
